d8fdfeb6720e87000b96aeace5ad47d0c1382ae7
katcozadd <krosecozadd@gmail.com>
2018-03-20 19:57:26 -0600

    Commit 9 - Created loops.js file

---
+++ resume_page/loops.js
@@ -0,0 +1,34 @@
+//Loop 1
+for (let i = 0; i <= 99; i++) {
+	console.log(i);
+}
+
+//Loop 2
+for (let i = 99; i >= 0; i--) {
+	console.log(i);
+}
+
+//Loop 3
+for (let i = 0; i <= 98; i+=2) {
+	console.log(i);
+}
+
+//Loop 4
+for (let i = 98; i >= 0; i-=2) {
+	console.log(i);
+}
+
+//Loop 5
+for (i = 49; i <= 72; i++) {
+	console.log(i);
+}
+
+//Loop 6
+for (i = 81; i >= 26; i--) {
+	console.log(i);
+}
+
+//Loop 7
+for (let i = 3; i <= 90; i+=3) {
+	console.log(i);
+}
